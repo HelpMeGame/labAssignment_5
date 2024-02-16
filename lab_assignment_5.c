d56685d0591f3cce2ba3c77ae752ace7f9cc364e
HelpMeGame <keagan.a.bowman@gmail.com>
2024-02-16 11:30:29 -0500

    Changed lab_assignment_5 code

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -9,15 +9,15 @@ typedef struct node {
 // Returns number of nodes in the linkedList.
 int length(node* head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+	int count = 0;
+	node* nextNode = head;
+	while (nextNode != NULL)
+	{
+		count++;
+		nextNode = nextNode->next;
+	}
 
-   return (len);
+	return count;
 }
 
 // parses the string in the linkedList
@@ -25,6 +25,30 @@ int length(node* head)
 //  then toCString function wil return "abc"
 char* toCString(node* head)
 {
+	// get size of linkedList
+	int count = length(head);
+
+	char* finalString;
+
+	// malloc final string
+	finalString = (char*)calloc(sizeof(char)*(count + 1), count + 1);
+	if (finalString == NULL)
+	{
+		exit(1);
+	}
+
+	// loop through nodes
+	node* nextNode = head;
+	for (int i = 0; i < count; i++)
+	{
+		finalString[i] = nextNode->letter;
+		nextNode = nextNode->next;
+	}
+
+	// add string terminator
+	finalString[count + 1] = "\0";
+
+	return finalString;
 }
 
 // inserts character to the linkedlist
@@ -33,11 +57,60 @@ char* toCString(node* head)
 // head -> |a|->|b|->|c|->|x|
 void insertChar(node** pHead, char c)
 {
+
+	// malloc new node
+	node* newNode = (node*)malloc(sizeof(node));
+	if (newNode == NULL)
+	{
+		exit(1);
+	}
+
+	newNode->letter = c;
+	newNode->next = NULL;
+
+	
+	node* tempOld = *pHead;
+
+	if (tempOld == NULL)
+	{
+		*pHead = newNode;
+	}
+	else
+	{
+		while (tempOld->next != NULL)
+		{
+			tempOld = tempOld->next;
+		}
+		tempOld->next = newNode;
+	}
+
+
 }
 
 // deletes all nodes in the linkedList.
 void deleteList(node** pHead)
 {
+	// create vals
+	node* next = *pHead;
+	node* prev = NULL;
+
+	// return if next is null
+	if (next == NULL)
+		return;
+
+	// loop through nodes, freeing prev
+	while (next->next != NULL)
+	{
+		prev = next;
+		next = next->next;
+		free(prev);
+	}
+
+	// free remaining node
+	if (next != NULL)
+		free(next);	
+
+	*pHead = NULL;
 }
 
 int main(void)
